4f65990d001c4f27946b8d07e955f302a50c3098
BlackSun559 <blacksun559@gmail.com>
2015-05-21 02:00:52 -0500

    Added mod class

---
+++ src/main/java/com/thewardedmen/thewardedblock/TheWardedBlock.java
@@ -0,0 +1,9 @@
+package com.thewardedmen.thewardedblock;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "TheWardedBlock", name = "The Warded Block", version = "1.8-1.0")
+public class TheWardedBlock
+{
+    
+}
