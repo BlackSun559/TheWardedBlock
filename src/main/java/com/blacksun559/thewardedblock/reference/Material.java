9bcb02bbeabced8088f15d4e756d112eda00e1ca
BlackSun559 <blacksun559@gmail.com>
2016-02-28 21:39:55 -0600

    Updating Tool Material
    Updated tool material to better reflect how it should work. Still WIP
    and bound to change as entities are added.

--- src/main/java/com/blacksun559/thewardedblock/reference/Material.java
+++ src/main/java/com/blacksun559/thewardedblock/reference/Material.java
@@ -7,7 +7,7 @@ public class Material
 {
     public static final class Tools
     {
-        public static final Item.ToolMaterial DEMON_BONE = EnumHelper.addToolMaterial(Names.Materials.DEMON_BONE, 3, 0, 14f, 5f, 0);
-        public static final Item.ToolMaterial WARD_SWORD = EnumHelper.addToolMaterial(Names.Materials.WARD_SWORD, 3, 0 ,16f, 7f, 0);
+        public static final Item.ToolMaterial DEMON_BONE = EnumHelper.addToolMaterial(Names.Materials.DEMON_BONE, 3, 1500, 12f, 5f, 15);
+        public static final Item.ToolMaterial WARD_SWORD = EnumHelper.addToolMaterial(Names.Materials.WARD_SWORD, 3, 2500, 16f, 10f, 25);
     }
 }
